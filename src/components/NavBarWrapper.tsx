80658cb272b51fa6834aa4adba2daaf66b15c72b
aZaaa1406 <rvaldeza2200@alumno.ipn.mx>
2025-04-02 01:44:45 -0600

    last commit

--- src/components/NavBarWrapper.tsx
+++ src/components/NavBarWrapper.tsx
@@ -6,7 +6,7 @@ import NavBarHome from "@/components/nav-bar-home";
 export default function NavbarWrapper() {
   const pathname = usePathname();
 
-  const isHomepage = pathname === "/" || pathname.startsWith("/formsAuth");
+  const isHomepage = pathname === "/" || pathname.startsWith("/formsAuth") || pathname.startsWith("/forgot-password") || pathname.startsWith("/reset-password") || pathname.startsWith("/unauthorized") || pathname.startsWith("/terms-and-conditions") || pathname.startsWith("/privacy-policy") || pathname.startsWith("/about-us") || pathname.startsWith("/contact-us") || pathname.startsWith("/reset-password");  
 
   return isHomepage ? <NavBarHome /> : null;
 }
